746ba998366b079f2564e886225f3b37e9f6dba6
thuan nhat <thuan nhat>
2017-03-20 04:10:19 +0700

    create history

---
+++ resource/js/history.js
@@ -0,0 +1,12 @@
+/**
+ * Created by thuan on 3/20/2017.
+ */
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        format:'dd/mm/yyyy'
+    });
+});
