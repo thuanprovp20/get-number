7928b214ceca9e2628bd620ce58696f824bff05b
thuan nhat <thuan nhat>
2017-03-22 01:43:38 +0700

    fix datepicker 1950 - 2018

--- resource/js/register.js
+++ resource/js/register.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
     $('select').material_select();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year
-        format:'dd/mm/yyyy'
+        selectYears: 100, // Creates a dropdown of 15 years to control year
+        format:'dd/mm/yyyy',
+        min:[1950,0,1],
+        max:[2018,1,1]
     });
 });
